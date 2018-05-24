a5ace45e7947476fc2337cbd790e31fa5f2ef236
Kamaia <krystalamaia@gmail.com>
2018-05-24 00:57:45 -0700

    Sound Notification Working.
    Signed-off-by: Kamaia <krystalamaia@gmail.com>

--- src/com/amaiaindustries/rw/test/RWPluginTest.java
+++ src/com/amaiaindustries/rw/test/RWPluginTest.java
@@ -1,6 +1,5 @@
 package com.amaiaindustries.rw.test;
 
-import com.amaiaindustries.rw.Utils.*;
 import net.risingworld.api.*;
 import net.risingworld.api.events.*;
 import net.risingworld.api.events.player.*;
@@ -20,41 +19,42 @@ public class RWPluginTest extends Plugin implements Listener {
 		registerEventListener(this);
 		System.out.println("Hello from Amaia Industries");
 	}
-	@EventMethod(Threading.Sync)
+
+	//This method commented out to be saved for a future release.
+	/*@EventMethod(Threading.Sync)
 	public void onPlayerHitEvent(PlayerDamageEvent phe){
 		System.out.println("Hit for: " + AIUtilities.convertShortToString(phe.getDamage()) + " damage");
 		phe.getPlayer().sendTextMessage("You Have Been Hit And Taken: "
 			                                   + phe.getDamage()
 			                                   + " Damage.");
 		phe.getPlayer().sendTextMessage("You now have: " + phe.getPlayer().getHealth() + " Health remaining.");
-	}
-
+	}*/
 	@EventMethod(Threading.Sync)
-	public void onPlayerObjectEvent(PlayerObjectEvent event) {
-		if (event.getObjectDefinition().isFurnace()) {
-			event.getPlayer().sendTextMessage("Status: " + AIUtilities.byteToString(event.getObjectStatus()));
+	public void onPlayerChangeObjectStatusEvent(PlayerChangeObjectStatusEvent event) {
+		Player p = event.getPlayer();
+		String name = event.getObjectDefinition().getName();
+		if (name == "bacon") {
+			p.sendTextMessage("Ding Fries Are Done!");
 		}
 	}
-
 	@EventMethod(Threading.Sync)
 	public void onPlayerObjectInteractionEvent(PlayerObjectInteractionEvent event) {
 		Player p = event.getPlayer();
 		byte b = event.getObjectStatus();
 		if (event.getObjectDefinition().isFurnace()) {
-			//check to see if we're activating the furnace and say Hello.
-			p.sendTextMessage("Hello");
 			Timer t = new Timer(360, 0, 1, () -> {
 				p.sendTextMessage("[#FF0000]Your Ore Is Now Done!");
-				SoundInformation si = new SoundInformation(getPath() + "/assets/sounds/ding.ogg");
+				SoundInformation si = new SoundInformation(this, "/assets/sounds/ding.ogg");
 				p.playSound(si);
 			});
 			if (b == 0x00) {
+				p.sendTextMessage("[#00FF00] Starting Ore Timer");
 				t.start();
 			}
 
 			if (b == 0x01) {
 				if (t.isActive()) {
-					p.sendTextMessage("[#00FF00]Stopping Timer");
+					p.sendTextMessage("[#00FF00]Stopping Ore Timer");
 					t.kill();
 				}
 

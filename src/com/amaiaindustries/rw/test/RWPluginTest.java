0c5865c4f87898cfb6fadce7e0ff27d1475b727f
Kamaia <krystalamaia@gmail.com>
2018-05-23 23:51:37 -0700

    Text Alert working.  Testing Sound.

--- src/com/amaiaindustries/rw/test/RWPluginTest.java
+++ src/com/amaiaindustries/rw/test/RWPluginTest.java
@@ -6,6 +6,7 @@ import net.risingworld.api.events.*;
 import net.risingworld.api.events.player.*;
 import net.risingworld.api.events.player.world.*;
 import net.risingworld.api.objects.*;
+import net.risingworld.api.utils.*;
 
 
 /**
@@ -38,10 +39,26 @@ public class RWPluginTest extends Plugin implements Listener {
 	@EventMethod(Threading.Sync)
 	public void onPlayerObjectInteractionEvent(PlayerObjectInteractionEvent event) {
 		Player p = event.getPlayer();
-		String s = "Hello";
+		byte b = event.getObjectStatus();
 		if (event.getObjectDefinition().isFurnace()) {
-			//check to see if we're activating the furnace
-			p.sendTextMessage(AIUtilities.byteToString(event.getObjectStatus()));
+			//check to see if we're activating the furnace and say Hello.
+			p.sendTextMessage("Hello");
+			Timer t = new Timer(360, 0, 1, () -> {
+				p.sendTextMessage("[#FF0000]Your Ore Is Now Done!");
+				SoundInformation si = new SoundInformation(getPath() + "/assets/sounds/ding.ogg");
+				p.playSound(si);
+			});
+			if (b == 0x00) {
+				t.start();
+			}
+
+			if (b == 0x01) {
+				if (t.isActive()) {
+					p.sendTextMessage("[#00FF00]Stopping Timer");
+					t.kill();
+				}
+
+			}
 		}
 	}
 
